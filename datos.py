929122e27918faae46872326ad0c2b39f7865c91
Unknown <if708833@iteso.mx>
2020-02-24 08:32:19 -0600

    datos

---
+++ datos.py
@@ -0,0 +1,6 @@
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: datos.py - datos generales para uso en el proyecto
+# -- mantiene: Alejandra Cortes
+# -- repositorio: https://github.com/alecortees22/LAB_2_ACS
+# -- ------------------------------------------------------------------------------------ -- #
