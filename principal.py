aa77427ba95d6fe2e4d1ecd21d3834878fb1f548
Unknown <if708833@iteso.mx>
2020-02-27 07:21:21 -0600

    texto principal

---
+++ principal.py
@@ -0,0 +1,6 @@
+# -- ------------------------------------------------------------------------------------ -- #
+# -- proyecto: Microestructura y Sistemas de Trading - Laboratorio 2 - Behavioral Finance
+# -- archivo: principal.py - Codigo principal para el proyecto
+# -- mantiene: Alejandra Cortes
+# -- repositorio: https://github.com/alecortees22/LAB_2_ACS
+# -- ------------------------------------------------------------------------------------ -- #
