605a93e246a1b72b089a78cba9bc028c5c393f11
Unknown <if708833@iteso.mx>
2020-03-12 11:08:37 -0600

    ranking

--- funciones.py
+++ funciones.py
@@ -9,7 +9,7 @@ import numpy as np
 
 # ----------------- Funcion para mandar llamar archivo y acomodarlo a mi conveniencia--------------
 def f_leer_archivo(param_archivo):
-    param_archivo = 'Statement_1.xlsx'
+    param_archivo = 'archivo_profe.xlsx'
     df_data = pd.read_excel('archivos/' + param_archivo, sheet_name='Statement')
     # elegir solo renglones en los que la columna type == buy | type == 'sell'
     df_data = df_data.loc[df_data['type']!='balance']
@@ -63,12 +63,12 @@ def f_estadisticas_ba(param_data):
     perdedoras = np.sum(param_data['profit'] < 0)
     perdedoras_c = (np.where(param_data['profit'] < 0, param_data['type'] == 'buy', 0).sum())
     perdedoras_v = (np.where(param_data['profit'] < 0, param_data['type'] == 'sell', 0).sum())
-    media_p = param_data['profit'].median()
-    media_pips = param_data['pips'].median()
-    r_efectividad = ops_totales/ganadoras
-    r_proporcion = ganadoras/perdedoras
-    r_efectividad_c = ops_totales/ganadoras_c
-    r_efectividad_v = ops_totales/ganadoras_v
+    media_p = np.trunc(param_data['profit'].median())
+    media_pips = np.trunc(param_data['pips'].median())
+    r_efectividad = (ops_totales/ganadoras)
+    r_proporcion = (ganadoras/perdedoras)
+    r_efectividad_c = (ops_totales/ganadoras_c)
+    r_efectividad_v = (ops_totales/ganadoras_v)
     pd_data = {'Medida': ['Ops Totales', 'Ganadoras', 'Ganadoras_c', 'Ganadoras_v', 'Perdedoras',
                                'Perdedoras_c', 'Perdedoras_v', 'Media(Profit)', 'Media(Pips)', 'r_efectividad',
                                'r_proporcion', 'r_efectividad_c', 'r_efectividad_v'],
@@ -82,14 +82,22 @@ def f_estadisticas_ba(param_data):
                                'Ganadoras Compras/Operaciones Totales', 'Ganadoras Ventas/Operaciones Totales']}
     df_1_tabla = pd.DataFrame(pd_data)
     # Creacion de DataFrame para los rankings
+    # Primero obtenemos un array que nos indique todos los tipos de instrumentos que tiene nuestro archivo
     instrumentos = np.unique(param_data['symbol'])
-    instrumentos_u = list()
-    for i in range(len(param_data.index)):
-        for j in range(len(instrumentos)):
-            if param_data['symbol'][i] == instrumentos[j]:
-                instrumentos_u.append()
+    # Después, indicamos que nuestra variable va a ser un DataFrame y vamos a rellenar los espacios
+    df_1_ranking = pd.DataFrame(columns=['Symbol', 'Rank'], index=np.array([i for i in range(0, len(instrumentos))]))
+    # Rellenamos la columna con los nombres de los simbolos
+    df_1_ranking['Symbol'] = [instrumentos[i] for i in range(0, len(instrumentos))]
+    # Ahora creamos los rankings
+    i = 0
+    for Symbol in instrumentos:
+        Ops_ganadas= sum(1 for i in param_data.index if param_data.loc[i, 'profit'] >= 0
+                      and param_data.loc[i, 'symbol'] == Symbol)
+        Ops_totales = sum(1 for i in param_data.index if param_data.loc[i, 'symbol'] == Symbol)
 
+        df_1_ranking.loc[i, 'Rank'] = str(np.round(Ops_ganadas / Ops_totales, 2)*100) + '%'
+        i += 1
 
     # Creacion del diccionario
-    dictionary = {'df_1_tabla': df_1_tabla}
-    return instrumentos
+    dictionary = {'df_1_tabla': df_1_tabla, 'df_1_ranking': df_1_ranking}
+    return dictionary
